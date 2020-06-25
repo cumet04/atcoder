48ae0c97bd60362f9761d157456f5c6f43c9fc08
cumet04 <medalhkr@gmail.com>
2020-06-25 10:46:25 +0900

    解説など見ながら

--- contests/abc170/d/main.rb
+++ contests/abc170/d/main.rb
@@ -3,59 +3,19 @@ AS = gets.split.map(&:to_i)
 
 as = AS.sort
 
-a = true
-results = 0
-while a
-  a = as.shift
-  break unless a
-  while as[0] == a # skip same numbers
-    a = as.shift
-    a = false if as.length == 0
-  end
-  break unless a
+max = as[-1]
+bmap = as.to_h { |n| [n, true] }
+bmap.default = false
 
-  nex = []
-  as.each do |n|
-    nex.push(n) unless n % a == 0
+as.each.with_index do |n, i|
+  next unless bmap[n]
+
+  v = n
+  while (v += n) <= max
+    bmap[v] = false
   end
-  results += 1
-  as = nex
+
+  bmap[n] = false if as[i + 1] == n
 end
 
-puts results
-
-# わからん
-
-# require "prime"
-
-# as = AS.sort.reverse
-# if as[-1] == 1
-#   puts AS.count - 1
-#   exit
-# end
-
-# count = N - 1
-# i = 0
-# while i < N
-#   a = as[i]
-#   while as[i + 1] == a # skip same numbers
-#     count -= 1
-#     a = as[i += 1]
-#   end
-
-#   if Prime.prime?(a)
-#     i += 1
-#     next
-#   end
-
-#   (1...N - i).each do |j|
-#     if a % as[-j] == 0
-#       count -= 1
-#       i += 1
-#       break
-#     end
-#   end
-#   i += 1
-# end
-
-# puts count
+puts bmap.values.select(&:itself).count

dddd0936430839eedaf6c07cf245bd3183809eec
cumet04 <medalhkr@gmail.com>
2020-06-25 10:47:55 +0900

    slim

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,10 @@
 # run in contests/abcXXX/{a-f}/
 
 ls -1 inouts/in* | while read FILE; do 
-  echo $FILE
+  echo "====="$FILE
   # 'head -c -1' for trim trailing newline; https://qiita.com/kkdd/items/35707fa38358ec8c17fc
-  actual=$(cat $FILE | time ruby main.rb | head -c -1)
+  actual=$(cat $FILE | ruby main.rb | head -c -1)
+  # actual=$(cat $FILE | time ruby main.rb | head -c -1)
   expected=$(echo $FILE | sed 's|inouts/in|inouts/out|' | xargs cat)
   if [[ "$actual" = "$expected" ]]; then
     echo "=> OK"
@@ -15,5 +16,4 @@ ls -1 inouts/in* | while read FILE; do
     echo "---"
     echo "$expected"
   fi
-  echo "====="
 done

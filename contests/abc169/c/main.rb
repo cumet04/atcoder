89f77cd7f5935d8662cc053553d17dfd1c188d1a
cumet04 <medalhkr@gmail.com>
2020-06-27 15:50:08 +0900

    回答より
    それはそう

--- contests/abc169/c/main.rb
+++ contests/abc169/c/main.rb
@@ -1,7 +1,15 @@
-require "bigdecimal"
-
 ASTR, BSTR = gets.split
-A = BigDecimal(ASTR)
-B = BigDecimal(BSTR)
+A = ASTR.to_i
+B = BSTR.to_f
 
-puts (A * B).floor
+puts (A * (B * 100).round / 100).floor
+
+# ---
+
+# require "bigdecimal"
+
+# ASTR, BSTR = gets.split
+# A = BigDecimal(ASTR)
+# B = BigDecimal(BSTR)
+
+# puts (A * B).floor

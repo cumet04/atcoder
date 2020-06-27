41fddebd5b720663f2cfa06185fd1a3626d6b373
cumet04 <medalhkr@gmail.com>
2020-06-27 15:59:28 +0900

    回答より

--- contests/abc169/d/main.rb
+++ contests/abc169/d/main.rb
@@ -2,34 +2,13 @@ N = gets.to_i
 
 require "prime"
 
-LIMIT = 10 ** 6
-
-zmap1 = {}
-zmap2 = {}
-Prime.each(LIMIT) do |p|
-  zmap1[p] = true
-  e = 2
-  while (z = p ** e) < LIMIT
-    zmap2[z] = true
-    e += 1
-  end
-end
-
-zs1 = zmap1.keys.sort.reverse
-zs2 = zmap2.keys.sort
-
-n = N
 count = 0
-
-(zs1 + zs2).each do |z|
-  if n > LIMIT && Prime.prime?(n)
+Prime.prime_division(N).each do |div|
+  p, n = div
+  pow = 1
+  while (n -= pow) >= 0
     count += 1
-    break
-  end
-
-  if n % z == 0
-    count += 1
-    n = n.div(z)
+    pow += 1
   end
 end
 

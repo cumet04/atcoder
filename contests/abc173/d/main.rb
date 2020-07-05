ecaffd6c940d925ee56b38bc90c589c5a57cb175
cumet04 <medalhkr@gmail.com>
2020-07-05 22:58:46 +0900

    終了後に回答見ずにやってみたらできた

--- contests/abc173/d/main.rb
+++ contests/abc173/d/main.rb
@@ -1,2 +1,14 @@
 N = gets.to_i
 AS = gets.split.map(&:to_i)
+
+sorted = AS.sort.reverse
+score = 0
+semimax_i = 0
+
+(sorted.length - 1).times.each do |i|
+  score += sorted[semimax_i]
+  semimax_i += 1 if i % 2 == 0
+  i += 1
+end
+
+puts score

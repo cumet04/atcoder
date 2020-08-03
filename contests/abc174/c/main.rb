828abfcb0b71d57336a80e56be1e1a907c74c574
cumet04 <medalhkr@gmail.com>
2020-08-03 16:47:57 +0900

    動画見て

--- contests/abc174/c/main.rb
+++ contests/abc174/c/main.rb
@@ -1,30 +1,14 @@
 K = gets.to_i
 
-# if [0, 2, 4, 5, 6, 8].include?(K[-1].to_i)
-#   puts "-1"
-#   exit
-# end
-
-a = K % 10
-amap = 10.times.map { |i|
-  [a * i % 10, i]
-}.to_h
-
-x = 0
-
-999983.times.each do |i|
-  a = K % 10 ** (i + 1)
-  b = a * x
-  c = (b / (10 ** i)) % 10
-  d = 7 - c
-  d = d >= 0 ? d : 10 + d
-  # puts "#{a} #{b} #{c} #{d}"
-  if amap[d].nil?
-    puts "-1"
+n = 0
+# 例3がありうる最大っぽいので999982超えてたらきっと大丈夫
+1_000_000.times.each do |i|
+  n = n * 10 + 7
+  if n % K == 0
+    puts i + 1
     exit
   end
-  x += 10 ** i * amap[d]
-  # puts x
+  n = n % K
 end
 
-# なんもわからん
+puts -1
